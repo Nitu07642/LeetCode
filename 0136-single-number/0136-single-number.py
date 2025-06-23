61a6bd321cadebad61aef44fb4003d4cedd6ff30
NITU KUMARI <nitu07642@gmail.com>
2025-06-23 11:00:30 +0530

    Time: 3 ms (65.74%), Space: 19.6 MB (77.58%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+      result=0
+      for i in range(0,len(nums)):
+         result= result ^ nums[i]
+      return result     
+
+
+
+
+
+
+
+
+
+
+
+
+        
